f6a5bf7d09a1b59d28b1a72765ae0bc9e5253492
Ethan-Garland <115115713+Ethan-Garland@users.noreply.github.com>
2022-11-08 14:06:08 -0600

    Create script.js

---
+++ U4_Intro to Javascript/4.4_Algorithims/script.js
@@ -0,0 +1,24 @@
+// step 1: define our variables with the let keyword
+// step 2: define a new variable for the evaluation of the formula
+// step 3: set our new variable = the value we are trying to find
+// step 4: Print our result to the console.
+
+// find the average of two numbers
+let number1 = 6;
+let number2 = 4;
+let average = (number1 + number2)/2;
+console.log("the average if the two numbers is " + average + ".");
+
+//find the area of a circle pi*r**2
+let radius = 5;
+let area = Math.PI*radius**2;
+console.log("The area of a circle is " + area + ".");
+
+//displacement is  Δy=.5at**2 (change in y equals .5 * acceleration * time squared)
+
+let acceleration = 10;
+let time = 5;
+let changeY = .5*acceleration*time**2;
+console.log("The Change in Y is " + changeY + ".");
+
+// solutions to 0 = ax**2 + bx + c are x = (-b +- sqrt(b**2 - 4ac))
