68091d45abffa21a916181c5dd66b15f71928930
Ethan-Garland <115115713+Ethan-Garland@users.noreply.github.com>
2022-11-08 14:04:58 -0600

    Create script.js

---
+++ U4_Intro to Javascript/4.4_Algorithims/Algorithims assignment/script.js
@@ -0,0 +1,19 @@
+// find the average of three numbers
+let number1 = 2;
+let number2 = 8;
+let number3 = 5;
+let average = (number1 + number2 + number3) / 3;
+console.log("the average if the three numbers is " + average + ".");
+
+// find the area of the triangle 
+let base = 12;
+let height = 4;
+let area = (base*height)*.5;
+console.log("The area if the triangle is " + area + ".")
+
+// find kinetic energy (k=mV**2/2)
+let mass = 6;
+let velocity = 2;
+let Kineticenergy = (mass*velocity**2) / 2;
+
+console.log("The kinetic energy is " + Kineticenergy + ".")
