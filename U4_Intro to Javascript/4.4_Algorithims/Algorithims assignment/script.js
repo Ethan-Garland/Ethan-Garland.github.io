34734ab5733fb95c855ad1481ede622304e2fef8
Ethan-Garland <115115713+Ethan-Garland@users.noreply.github.com>
2022-11-10 14:51:16 -0600

    Update script.js

--- U4_Intro to Javascript/4.4_Algorithims/Algorithims assignment/script.js
+++ U4_Intro to Javascript/4.4_Algorithims/Algorithims assignment/script.js
@@ -1,19 +1,24 @@
-// find the average of three numbers
-let number1 = 2;
-let number2 = 8;
-let number3 = 5;
-let average = (number1 + number2 + number3) / 3;
-console.log("the average if the three numbers is " + average + ".");
+// step 1: define our variables with the let keyword
+// step 2: define a new variable for the evaluation of the formula
+// step 3: set our new variable = the value we are trying to find
+// step 4: Print our result to the console.
 
-// find the area of the triangle 
-let base = 12;
-let height = 4;
-let area = (base*height)*.5;
-console.log("The area if the triangle is " + area + ".")
+// find the average of two numbers
+let number1 = 6;
+let number2 = 4;
+let average = (number1 + number2)/2;
+console.log("the average if the two numbers is " + average + ".");
 
-// find kinetic energy (k=mV**2/2)
-let mass = 6;
-let velocity = 2;
-let Kineticenergy = (mass*velocity**2) / 2;
+//find the area of a circle pi*r**2
+let radius = 5;
+let area = Math.PI*radius**2;
+console.log("The area of a circle is " + area + ".");
 
-console.log("The kinetic energy is " + Kineticenergy + ".")
+//displacement is  Δy=.5at**2 (change in y equals .5 * acceleration * time squared)
+
+let acceleration = 10;
+let time = 5;
+let changeY = .5*acceleration*time**2;
+console.log("The Change in Y is " + changeY + ".");
+
+// solutions to 0 = ax**2 + bx + c are x = (-b +- sqrt(b**2 - 4ac))
