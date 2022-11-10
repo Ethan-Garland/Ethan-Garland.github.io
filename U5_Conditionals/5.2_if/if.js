1a739c84a59542d7fbb322f6faf1de0b22ae4572
Ethan-Garland <115115713+Ethan-Garland@users.noreply.github.com>
2022-11-10 14:48:22 -0600

    Create if.js

---
+++ U5_Conditionals/5.2_if/if.js
@@ -0,0 +1,31 @@
+// Our first conditional statement. Check if a person is 18 years old or older\
+let age = 20;
+if(age >= 18) {
+    console.log("You are old enough to vote!");
+}
+console.log("Thank you for checking your age!");
+
+// Check if you can go to the mall. grades must be >= 90
+let comp_sci_grade = 95;
+if (comp_sci_grade >= 90) {
+    console.log("You can go to the mall!");
+}
+
+// Check driving age. If your age is less than 16 you cannot drive.
+age = 14
+if (age < 16){
+    console.log("sorry you are not old enough to drive.");
+}
+
+
+//What will print to the console in the following code? 
+
+let height = 50;
+
+if (height < 60){
+
+    console.log("Sorry, You are not tall enough to ride");
+
+}
+
+console.log("Thank you for visitng the ammusment park!");
